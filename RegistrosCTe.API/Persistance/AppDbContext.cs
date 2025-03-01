48a54c8776a47020768544a62bed9313666282d6
murilojcavalcanti <josemurilo7@gmail.com>
2025-03-01 15:54:30 -0300

    Dev:Criando Context

---
+++ RegistrosCTe.API/Persistance/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosCTe.Domain.Entities;
+
+namespace RegistrosCTe.API.Persistance
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext>opts):base(opts) { }
+
+        DbSet<Viagem> Viagems { get; set; }
+        DbSet<Carga> Carga { get; set; }
+        DbSet<DespesaAdicional> DespesasAdicionais { get; set; }
+        
+    }
+}

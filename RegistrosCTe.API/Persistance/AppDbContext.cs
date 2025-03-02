bdef7cb5d546158a7bf2f93e8a8c3f04ad551349
murilojcavalcanti <josemurilo7@gmail.com>
2025-03-02 08:05:37 -0300

    bugFix:Corrigindo modificador de acesso dbContext

--- RegistrosCTe.API/Persistance/AppDbContext.cs
+++ RegistrosCTe.API/Persistance/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace RegistrosCTe.API.Persistance
 {
     public class AppDbContext : DbContext
     {
-        protected AppDbContext(DbContextOptions<AppDbContext>opts):base(opts) { }
+        public AppDbContext(DbContextOptions<AppDbContext>opts):base(opts) { }
 
         DbSet<Viagem> Viagems { get; set; }
         DbSet<Carga> Carga { get; set; }

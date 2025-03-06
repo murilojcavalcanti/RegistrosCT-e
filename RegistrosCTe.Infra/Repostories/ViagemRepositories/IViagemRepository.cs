5b74c73d60c974cb1e2e58fc09a9aef683e1a09b
murilojcavalcanti <josemurilo7@gmail.com>
2025-03-05 22:01:23 -0300

    DEV: Criando ViagemRepository

---
+++ RegistrosCTe.Infra/Repostories/ViagemRepositories/IViagemRepository.cs
@@ -0,0 +1,22 @@
+﻿using RegistrosCTe.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrosCTe.Infra.Repostories.ViagemRepositories
+{
+    public interface IViagemRepository
+    {
+        Viagem Post(Viagem model);
+
+        List<Viagem> GetAll();
+
+        Viagem GetById(int id);
+
+        void Update(Viagem viagem);
+        
+        void Delete(Viagem viagem);
+    }
+}
---
+++ RegistrosCTe.Infra/Repostories/ViagemRepositories/ViagemRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrosCTe.API.Persistance;
+using RegistrosCTe.Domain.Entities;
+
+namespace RegistrosCTe.Infra.Repostories.ViagemRepositories
+{
+    public class ViagemRepository:IViagemRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ViagemRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Viagem Post(Viagem viagem)
+        {
+            _context.Set<Viagem>().Add(viagem);
+            _context.SaveChanges();
+            return viagem;
+        }
+
+        public List<Viagem> GetAll()
+        {
+            List<Viagem> viagens = _context.Set<Viagem>().ToList();
+            if (viagens == null) throw new Exception("Viagens não existem");
+            return viagens;
+        }
+
+        public Viagem GetById(int id)
+        {
+            Viagem viagem = _context.Set<Viagem>()
+                .Include(v => v.CTe)
+                .Include(v => v.Carga).Include(v => v.DespesaAdicionais)
+                .SingleOrDefault(v => v.Id == id);
+            if (viagem == null) throw new Exception("Viagem não existe");
+            return viagem;
+        }
+
+        public void Update(Viagem viagemUpdated)
+        {
+            _context.Update(viagemUpdated);
+            _context.SaveChanges();
+        }
+
+        public void Delete(Viagem viagem)
+        {   
+            _context.Remove(viagem);
+            _context.SaveChanges();
+        }
+    }
+}
